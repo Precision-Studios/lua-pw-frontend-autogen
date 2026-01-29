d5635ea8b23f57526f2c3b13d087237f0a08bb81
ExploiTR <pratimmajumder1@gmail.com>
2026-01-29 20:39:48 +0530

    rewrite for redirect.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:shortCode([a-zA-Z0-9]{1,8})",
+        destination: "http://localhost:5000/:shortCode",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
