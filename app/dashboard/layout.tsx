42d95e82d4d113c7bd80a3f81226b5eb6dc22a45
Pratim Majumder <ExploiTR@users.noreply.github.com>
2026-02-05 17:17:27 +0530

    feat: Add initial dashboard page, including its layout, styling, and an OpenAPI specification.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -24,6 +24,9 @@ export default function DashboardLayout({
                 >
                     <TopNav />
                     {children}
+                    <div className="dash-attribution">
+                        Photo by <a href="https://unsplash.com/@zamax?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Gustavo Zambelli</a> on <a href="https://unsplash.com/photos/night-sky-filled-with-stars-and-silhouettes--JMK4lyhnGM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
                 </div>
             </div>
         </UserProvider>
